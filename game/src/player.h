3c38ab718b185c3ea5c52a4182667a2ef8adbac8
Paraross <Adrago@onet.pl>
2024-04-08 21:34:56 +0200

    Remove unnecessary Player constructor

--- game/src/player.h
+++ game/src/player.h
@@ -7,6 +7,7 @@ struct Player {
     Color color;
     // Vector2 velocity;
     float max_speed;
+    float fire_cooldown;
 
     Player()
      : Player(0.0f, 0.0f, 40.0f, 40.0f, 1000.0f, BLACK) {}
@@ -21,10 +22,6 @@ struct Player {
         this->rect.height = height;
         this->color = color;
         this->max_speed = max_speed;
-    }
-
-    static auto init(float pos_x, float pos_y, float width, float height, float max_speed, Color color) -> Player {
-        return Player(pos_x, pos_y, width, height, max_speed, color);
     }
 
     static auto at_position(float pos_x, float pos_y) -> Player {
