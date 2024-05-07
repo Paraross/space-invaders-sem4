27442662f8620f1f46f636526aca7fdffa03ae6b
Paraross <Adrago@onet.pl>
2024-05-07 15:13:35 +0200

    Add system to draw text

--- game/src/draw.hpp
+++ game/src/draw.hpp
@@ -11,3 +11,16 @@ void draw_color_rectangles(entt::registry &registry) {
         DrawRectangleRec(rect.rect, color.color);
     }
 }
+
+void draw_text(entt::registry &registry) {
+    auto texts = registry.view<const TextComp>();
+
+    for (const auto [_, text_comp] : texts.each()) {
+        auto &text = text_comp.text;
+        auto &position = text_comp.position;
+        auto &font_size = text_comp.font_size;
+        auto &color = text_comp.color;
+
+        DrawText(text.c_str(), (int)position.x, (int)position.y, font_size, color);
+    }
+}
