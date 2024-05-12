902c9778cb64d608a029a419aa366b7802913b18
Paraross <Adrago@onet.pl>
2024-05-12 14:22:31 +0200

    Rename Input to Keybind, allow for 2 inputs per keybind

--- game/src/input.hpp
+++ game/src/input.hpp
@@ -1,6 +1,6 @@
 #include "raylib.h"
 
-class Input {
+class Keybind {
     // prywatne structy? nie we wspaniałym języku zwanym c++
     enum class InputType {
         Keyboard,
@@ -12,43 +12,54 @@ class Input {
         MouseButton mouse_button;
     };
 
-    InputType type;
-    KbOrM input;
+    InputType type[2];
+    KbOrM input[2];
 
-public:
-    Input(KeyboardKey key)
-        : type(InputType::Keyboard) {
-        input.keyboard_key = key;
+    auto is_in_state(bool (*fn)(int)) -> bool {
+        auto first_in_state = type[0] == InputType::Keyboard ? fn(input[0].keyboard_key) : fn(input[0].mouse_button);
+        auto second_in_state = type[1] == InputType::Keyboard ? fn(input[1].keyboard_key) : fn(input[1].mouse_button);
+
+        return first_in_state || second_in_state;
     }
 
-    Input(MouseButton button)
-        : type(InputType::Mouse) {
-        input.mouse_button = button;
+public:
+    Keybind(KeyboardKey key) {
+        type[0] = InputType::Keyboard;
+        type[1] = InputType::Keyboard;
+        input[0].keyboard_key = key;
+        input[1].keyboard_key = KeyboardKey::KEY_NULL;
+    }
+
+    Keybind(MouseButton button) {
+        type[0] = InputType::Mouse;
+        type[1] = InputType::Keyboard;
+        input[0].mouse_button = button;
+        input[1].keyboard_key = KeyboardKey::KEY_NULL;
     }
 
     auto is_down() -> bool {
-        return type == InputType::Keyboard ? IsKeyDown(input.keyboard_key) : IsMouseButtonDown(input.mouse_button);
+        return is_in_state(IsKeyDown);
     }
 
     auto is_up() -> bool {
-        return type == InputType::Keyboard ? IsKeyUp(input.keyboard_key) : IsMouseButtonUp(input.mouse_button);
+        return is_in_state(IsKeyUp);
     }
 
     auto is_pressed() -> bool {
-        return type == InputType::Keyboard ? IsKeyPressed(input.keyboard_key) : IsMouseButtonPressed(input.mouse_button);
+        return is_in_state(IsKeyPressed);
     }
 
     auto is_released() -> bool {
-        return type == InputType::Keyboard ? IsKeyReleased(input.keyboard_key) : IsMouseButtonReleased(input.mouse_button);
+        return is_in_state(IsKeyReleased);
     }
 };
 
 struct Keybinds {
-    Input move_right;
-    Input move_left;
-    Input move_up;
-    Input move_down;
-    Input shoot;
+    Keybind move_right;
+    Keybind move_left;
+    Keybind move_up;
+    Keybind move_down;
+    Keybind shoot;
 
     Keybinds()
         : move_right(KEY_RIGHT), move_left(KEY_LEFT), move_up(KEY_UP), move_down(KEY_DOWN), shoot(KEY_SPACE) {}
